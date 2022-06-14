44b435e7fe126eaf1d2f7b72e7fed3cc4978ea9d
enguelbertg13 <enguelbertg13@gmail.com>
2022-06-14 17:34:06 -0300

    cambio ejercicio 3

--- CSharpLesson4/Ejercicio3/Program.cs
+++ CSharpLesson4/Ejercicio3/Program.cs
@@ -2,16 +2,41 @@
 Console.WriteLine("Hello, World!");
 
 Console.WriteLine("Ingrese ancho");
-var ancho = Convert.ToInt16(Console.ReadLine());
+int ancho = Convert.ToInt16(Console.ReadLine());
 
 Console.WriteLine("Ingrese alto");
-var alto = Convert.ToInt16(Console.ReadLine());
+int alto = Convert.ToInt16(Console.ReadLine());
+
+Console.WriteLine("tiene relleno? 0/1");
+int relleno = Convert.ToInt16(Console.ReadLine());
+
 
 for (int j = 0; j < alto; j++)
 {
     for (int i = 0; i < ancho; i++)
     {
-        Console.Write("*");
+        if (ancho != alto)
+        {
+            if (i == (ancho / 2))
+            {
+                if (relleno == 0)
+                {
+                    Console.Write(" ");
+                }
+                else
+                {
+                    Console.Write("*");
+                }
+            }
+            else
+            {
+                Console.Write("*");
+            }
+        }
+        else
+        {
+            Console.Write("*");
+        }
     }
     Console.WriteLine();
 }
